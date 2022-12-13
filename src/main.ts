7258cff07cdb8396051720960b14396fb3f3a40d
叶继伟 <yejw6@asiainfo.com>
2022-12-13 22:38:26 +0800

    Element Plus 图标引入

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,11 @@ import '@/assets/css/index.less'
 import router from '@/router'
 import pinia from '@/store'
 
+import registerIcons from './global/register-icons'
+
 const app = createApp(App)
 
+app.use(registerIcons)
 app.use(pinia)
 app.use(router)
 
