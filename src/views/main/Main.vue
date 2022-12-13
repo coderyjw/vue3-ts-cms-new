ab8f081af1750951e12b98c5d1be361cd5c6f39d
叶继伟 <yejw6@asiainfo.com>
2022-12-14 02:16:23 +0800

    退出登录逻辑

--- src/views/main/Main.vue
+++ src/views/main/Main.vue
@@ -2,7 +2,17 @@
   <div class="main">main</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+import { LOGIN_TOKEN } from '@/global/constants'
+import { localCache } from '@/utils/cache'
+
+const router = useRouter()
+function handleExitClick() {
+  localCache.removeCache(LOGIN_TOKEN)
+  router.push('/login')
+}
+</script>
 
 <style lang="less" scoped>
 .main {
