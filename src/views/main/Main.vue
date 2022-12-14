bf0e78d7d2eb0d4b311062b110d896d9578d2b53
叶继伟 <yejw6@asiainfo.com>
2022-12-15 02:37:21 +0800

    首页整体布局（菜单树 头部布局）

--- src/views/main/Main.vue
+++ src/views/main/Main.vue
@@ -1,23 +1,58 @@
 <template>
   <div class="main">
-    main
-    <el-button @click="handleExitClick">退出登录</el-button>
+    <el-container class="main-content">
+      <el-aside :width="isFold ? '60px' : '210px'">
+        <main-menu :is-fold="isFold" />
+      </el-aside>
+      <el-container>
+        <el-header height="50px">
+          <main-header @fold-change="handleFoldChange" />
+        </el-header>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useRouter } from 'vue-router'
-import { LOGIN_TOKEN } from '@/global/constants'
-import { localCache } from '@/utils/cache'
+import { ref } from 'vue'
 
-const router = useRouter()
-function handleExitClick() {
-  localCache.removeCache(LOGIN_TOKEN)
-  router.push('/login')
+// 处理main-header中折叠的变化
+const isFold = ref(false)
+function handleFoldChange(flag: boolean) {
+  isFold.value = flag
 }
 </script>
 
 <style lang="less" scoped>
 .main {
+  height: 100%;
+}
+
+.main-content {
+  height: 100%;
+
+  .el-aside {
+    overflow-x: hidden;
+    overflow-y: auto;
+    line-height: 200px;
+    text-align: left;
+    cursor: pointer;
+    background-color: #001529;
+    scrollbar-width: none; /* firefox */
+    -ms-overflow-style: none; /* IE 10+ */
+
+    transition: width 0.3s ease;
+
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  }
+
+  .el-main {
+    background-color: #f0f2f5;
+  }
 }
 </style>
