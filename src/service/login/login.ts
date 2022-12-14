0dd3c80648eb8dffaa2d9f77eba1270a81e0c282
叶继伟 <yejw6@asiainfo.com>
2022-12-15 02:13:43 +0800

    用户信息获取

--- src/service/login/login.ts
+++ src/service/login/login.ts
@@ -7,3 +7,15 @@ export function accountLoginRequest(account: IAccount) {
     data: account
   })
 }
+
+export function getUserInfoById(id: number) {
+  return request.get({
+    url: `/users/${id}`
+  })
+}
+
+export function getUserMenusByRoleId(id: number) {
+  return request.get({
+    url: `/role/${id}/menu`
+  })
+}
--- src/store/login/login.ts
+++ src/store/login/login.ts
@@ -1,5 +1,9 @@
 import { defineStore } from 'pinia'
-import { accountLoginRequest } from '@/service/login/login'
+import {
+  accountLoginRequest,
+  getUserInfoById,
+  getUserMenusByRoleId
+} from '@/service/login/login'
 import type { IAccount } from '@/types'
 import { localCache } from '@/utils/cache'
 import { LOGIN_TOKEN } from '@/global/constants'
@@ -7,22 +11,33 @@ import router from '@/router'
 
 const useLoginStore = defineStore('login', {
   state: () => ({
-    id: '',
     token: localCache.getCache(LOGIN_TOKEN) ?? '',
-    name: ''
+    userInfo: localCache.getCache('userInfo') ?? {},
+    userMenus: localCache.getCache('userMenus') ?? []
   }),
   actions: {
     async loginAccountAction(account: IAccount) {
       // 1.账号登录, 获取token等信息
       const loginResult = await accountLoginRequest(account)
-      this.id = loginResult.data.id
-      this.name = loginResult.data.name
+      const id = loginResult.data.id
       this.token = loginResult.data.token
-
-      // 2.进行本地缓存
       localCache.setCache(LOGIN_TOKEN, this.token)
 
-      // 3.页面跳转(main页面)
+      // 2.获取登录用户的详细信息(role信息)
+      const userInfoResult = await getUserInfoById(id)
+      const userInfo = userInfoResult.data
+      this.userInfo = userInfo
+
+      // 3.根据角色请求用户的权限(菜单menus)
+      const userMenusResult = await getUserMenusByRoleId(this.userInfo.role.id)
+      const userMenus = userMenusResult.data
+      this.userMenus = userMenus
+
+      // 4.进行本地缓存
+      localCache.setCache('userInfo', userInfo)
+      localCache.setCache('userMenus', userMenus)
+
+      // 5.页面跳转(main页面)
       router.push('/main')
     }
   }
